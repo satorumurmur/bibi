a7f7ab8d3f3623bff0133f28d05545e4316bda59
Satoru MATSUSHIMA (℠) <soarstre@gmail.com>
2015-07-15 00:30:49 +0900

    Fix a bug on the laying-out of pre-paginated spreads in reflowable book in vertical-spread-layout-axis mode.

--- dev-bib/i/res/scripts/bibi.core.js
+++ dev-bib/i/res/scripts/bibi.core.js
@@ -1368,7 +1368,7 @@ R.resetItem.asPrePaginatedItem = function(Item) {
 			PageB / SpreadViewPort[S.SIZE.b],
 			PageL / SpreadViewPort[S.SIZE.l]
 		);
-        if(SpreadViewPort[S.SIZE.b] * Scale < PageB) Scale = PageB / SpreadViewPort[S.SIZE.b];
+        if(S.SLA != "vertical" && SpreadViewPort[S.SIZE.b] * Scale < PageB) Scale = PageB / SpreadViewPort[S.SIZE.b];
 		if(Item.Pair) Item.Pair.Scale = Scale;
 	}
 	PageL = Math.floor(ItemRef["viewport"][S.SIZE.l] * Scale);
