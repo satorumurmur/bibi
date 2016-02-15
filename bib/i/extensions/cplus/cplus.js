3c16fabb26b1f4a41ca443f32d465b00e254ea4e
Satoru MATSUSHIMA (℠) <soarstre@gmail.com>
2016-02-15 17:43:40 +0900

    Update compiled CSS/JavaScripts.

--- bib/i/extensions/cplus/cplus.js
+++ bib/i/extensions/cplus/cplus.js
@@ -8,11 +8,10 @@
  *
  * ## Components:
  * 1. BiB/i Extension: C+ViewMenu
- * 2. BiB/i Extension: C+Fullscreen
- * 3. BiB/i Extension: C+Arrows
- * 4. BiB/i Extension: C+Keys
- * 5. BiB/i Extension: C+Messages
- * 6. BiB/i Extension: C+Indicator
+ * 2. BiB/i Extension: C+Arrows
+ * 3. BiB/i Extension: C+Keys
+ * 4. BiB/i Extension: C+Messages
+ * 5. BiB/i Extension: C+Indicator
  */
 /*!
  *
@@ -21,14 +20,7 @@
  * - Copyright (c) Satoru MATSUSHIMA - http://bibi.epub.link/ or https://github.com/satorumurmur/bibi
  * - Licensed under the MIT license. - http://www.opensource.org/licenses/mit-license.php
  */
-Bibi.x({name:"C+ViewMenu",description:"Menu Buttons to Change View Mode.",author:"Satoru MATSUSHIMA (@satorumurmur)",version:Bibi.version,build:Bibi.build})(function(){if(!S["reader-view-mode-fixed"]){var e={};e.Item='<span class="bibi-shape bibi-shape-item"></span>',e.Spread='<span class="bibi-shape bibi-shape-spread">'+e.Item+e.Item+"</span>",e.SpreadsP='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-paged">'+e.Spread+e.Spread+e.Spread+"</span>",e.SpreadsH='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-horizontal">'+e.Spread+e.Spread+e.Spread+"</span>",e.SpreadsV='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-vertical">'+e.Spread+e.Spread+e.Spread+"</span>",C.addButton({Category:"menu",Group:"view",Labels:[{ja:"ページ表示",en:"Paged View"}],IconHTML:'<span class="bibi-icon bibi-icon-paged">'+e.SpreadsP+"</span>"},function(){C.Panel.toggle(function(){R.changeView("paged")})}),C.addButton({Category:"menu",Group:"view",Labels:[{ja:"横スクロール表示",en:"Scroll View (Horizontal)"}],IconHTML:'<span class="bibi-icon bibi-icon-horizontal">'+e.SpreadsH+"</span>"},function(){C.Panel.toggle(function(){R.changeView("horizontal")})}),C.addButton({Category:"menu",Group:"view",Labels:[{ja:"縦スクロール表示",en:"Scroll View (Vertical)"}],IconHTML:'<span class="bibi-icon bibi-icon-vertical">'+e.SpreadsV+"</span>"},function(){C.Panel.toggle(function(){R.changeView("vertical")})}),E.dispatch("bibi:createViewMenu")}}),/*!
- *
- * # BiB/i Extension: C+Fullscreen
- *
- * - Copyright (c) Satoru MATSUSHIMA - http://bibi.epub.link/ or https://github.com/satorumurmur/bibi
- * - Licensed under the MIT license. - http://www.opensource.org/licenses/mit-license.php
- */
-Bibi.x({name:"C+Fullscreen",description:"Floating Button for Switching Fullscreen.",author:"Satoru MATSUSHIMA (@satorumurmur)",version:Bibi.version,build:Bibi.build})(function(){!O.WindowEmbedded&&O.FullscreenEnabled&&(C["switch"].Fullscreen=C.addButton({id:"bibi-switch-fullscreen",Category:"switch",Group:"fullscreen",Labels:[{ja:"フルスクリーンモードを開始",en:"Enter Fullscreen"},{ja:"フルスクリーンモードを終了",en:"Exit Fullscreen"}],IconHTML:'<span class="bibi-icon bibi-switch bibi-switch-fullscreen"></span>'},function(){sML.getFullscreenElement()?(sML.exitFullscreen(),C.setLabel(C["switch"].Fullscreen,0),E.dispatch("bibi:exitFullscreen")):(sML.requestFullscreen(O.HTML),C.setLabel(C["switch"].Fullscreen,1),E.dispatch("bibi:enterFullscreen"))}),E.dispatch("bibi:createFullscreenSwitch"))}),/*!
+Bibi.x({name:"C+ViewMenu",description:"Menu Buttons to Change View Mode.",author:"Satoru MATSUSHIMA (@satorumurmur)",version:Bibi.version,build:Bibi.build})(function(){if(!S["reader-view-mode-fixed"]){var e={};e.Item='<span class="bibi-shape bibi-shape-item"></span>',e.Spread='<span class="bibi-shape bibi-shape-spread">'+e.Item+e.Item+"</span>",e.SpreadsP='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-paged">'+e.Spread+e.Spread+e.Spread+"</span>",e.SpreadsH='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-horizontal">'+e.Spread+e.Spread+e.Spread+"</span>",e.SpreadsV='<span class="bibi-shape bibi-shape-spreads bibi-shape-spreads-vertical">'+e.Spread+e.Spread+e.Spread+"</span>",C.addButton({Category:"panel-menus-alpha",Group:"view",Labels:[{ja:"ページ表示",en:"Paged View"}],IconHTML:'<span class="bibi-icon bibi-icon-paged">'+e.SpreadsP+"</span>"},function(){C.Panel.toggle(function(){R.changeView("paged")})}),C.addButton({Category:"panel-menus-alpha",Group:"view",Labels:[{ja:"横スクロール表示",en:"Scroll View (Horizontal)"}],IconHTML:'<span class="bibi-icon bibi-icon-horizontal">'+e.SpreadsH+"</span>"},function(){C.Panel.toggle(function(){R.changeView("horizontal")})}),C.addButton({Category:"panel-menus-alpha",Group:"view",Labels:[{ja:"縦スクロール表示",en:"Scroll View (Vertical)"}],IconHTML:'<span class="bibi-icon bibi-icon-vertical">'+e.SpreadsV+"</span>"},function(){C.Panel.toggle(function(){R.changeView("vertical")})}),E.dispatch("bibi:createViewMenu")}}),/*!
  *
  * # BiB/i Extension: C+Arrows
  *
@@ -42,7 +34,7 @@ Bibi.x({name:"C+Arrows",description:"Floating Buttons for Scrolling and Page-Fli
  * - Copyright (c) Satoru MATSUSHIMA - http://bibi.epub.link/ or https://github.com/satorumurmur/bibi
  * - Licensed under the MIT license. - http://www.opensource.org/licenses/mit-license.php
  */
-Bibi.x({name:"C+Keys",description:"Listening Key Pressing.",author:"Satoru MATSUSHIMA (@satorumurmur)",version:Bibi.version,build:Bibi.build})(function(){C.listenKeys=function(e){if(R.Started){e.preventDefault();var i=parent!=window&&parent.Bibi?parent:window;i.C.KeyCode=e.keyCode;var r=null;if("rtl"==S["page-progression-direction"])switch(e.keyCode){case 37:r=1;break;case 38:r=-1;break;case 39:r=-1;break;case 40:r=1}else switch(e.keyCode){case 37:r=-1;break;case 38:r=-1;break;case 39:r=1;break;case 40:r=1}r&&i.R.page(r)}},E.add("bibi:loadItem",function(e){e.contentWindow.addEventListener("keydown",C.listenKeys,!1)}),E.add("bibi:start",function(){O.SmartPhone||window.addEventListener("keydown",C.listenKeys,!1)})}),/*!
+Bibi.x({name:"C+Keys",description:"Listening Key Pressing.",author:"Satoru MATSUSHIMA (@satorumurmur)",version:Bibi.version,build:Bibi.build})(function(){C.listenKeys=function(e){if(R.Started){e.preventDefault();var i=window.Bibi&&window.O&&window.C?window:parent;i.C.KeyCode=e.keyCode;var r=null;if("rtl"==S["page-progression-direction"])switch(e.keyCode){case 37:r=1;break;case 38:r=-1;break;case 39:r=-1;break;case 40:r=1}else switch(e.keyCode){case 37:r=-1;break;case 38:r=-1;break;case 39:r=1;break;case 40:r=1}r&&i.R.page(r)}},E.add("bibi:loadItem",function(e){e.contentWindow.addEventListener("keydown",C.listenKeys,!1)}),E.add("bibi:start",function(){O.SmartPhone||window.addEventListener("keydown",C.listenKeys,!1)})}),/*!
  *
  * # BiB/i Extension: C+Messages
  *
