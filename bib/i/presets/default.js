1660166b4f0b8749864f5201f1f0a4a095b27cb0
Satoru MATSUSHIMA (℠) <soarstre@gmail.com>
2016-02-17 16:51:25 +0900

    Update preset and its README:
    1. Make object in default.js confirm to JSON format. (and add some item.)
    2. Update README.md for presets.

--- bib/i/presets/default.js
+++ bib/i/presets/default.js
@@ -2,45 +2,47 @@ P.initialize({
 
 // =================================================================================================
 
-'name'                         : 'BiB/i Default', // Name of this preset. As you like.
-'description'                  : 'Default Preset.', // Description of this preset. As you like.
+"preset-name"                  : "Default", // Name of this preset. As you like.
+"preset-description"           : "Default Preset for BiB/i.", // Description for this preset. As you like.
+"preset-author"                : "Satoru MATSUSHIMA", // Name of the author of this preset. As you like.
+"preset-author-href"           : "http://bibi.epub.link", // URI of a website, etc. of the author of this preset. As you like.
 
 // -------------------------------------------------------------------------------------------------
 
-'bookshelf'                    : '../../bib/bookshelf/', // relative path from bib/i/index.html or COR-allowed URI begins with 'http://' or 'https://'.
+"bookshelf"                    : "../../bib/bookshelf/", // relative path from bib/i/index.html or COR-allowed URI begins with "http://" or "https://".
 
-'reader-view-mode'             : 'paged', // 'paged' or 'vertical' or 'horizontal' ('paged' is for flipping, 'vertical' and 'horizontal' are for scrolling)
-'reader-view-mode-fixed'       : false, // true or false
+"reader-view-mode"             : "paged", // "paged" or "vertical" or "horizontal" ("paged" is for flipping, "vertical" and "horizontal" are for scrolling)
+"reader-view-mode-fixed"       : false, // true or false
 
-'play-with-new-window'         : 'handheld', // '' or 'always' or 'handheld'
+"play-with-new-window"         : "handheld", // "" or "always" or "handheld"
 
-'ui-font-family'               : '', // CSS font-family value as '"Helvetica", sans-serif' or ''
+"ui-font-family"               : "", // CSS font-family value as "'Helvetica', sans-serif" or ""
 
-'book-background'              : '', // CSS background value or ''
+"book-background"              : "", // CSS background value or ""
 
-'spread-gap'                   : 2, // px (if you set odd-number, it will be added 1.)
-'spread-margin-start'          : 0, // px
-'spread-margin-end'            : 0, // px
+"spread-gap"                   : 2, // px (if you set odd-number, it will be added 1.)
+"spread-margin-start"          : 0, // px
+"spread-margin-end"            : 0, // px
 
-'spread-border-radius'         : '', // CSS border-radius value or ''
-'spread-box-shadow'            : '', // CSS box-shadow value or ''
+"spread-border-radius"         : "", // CSS border-radius value or ""
+"spread-box-shadow"            : "", // CSS box-shadow value or ""
 
-'item-padding-left'            : 28, // px
-'item-padding-right'           : 28, // px
-'item-padding-top'             : 36, // px
-'item-padding-bottom'          : 20, // px
+"item-padding-left"            : 28, // px
+"item-padding-right"           : 28, // px
+"item-padding-top"             : 36, // px
+"item-padding-bottom"          : 20, // px
 
-'item-column-rule'             : '', // CSS column-rule value or ''
+"item-column-rule"             : "", // CSS column-rule value or ""
 
-'page-breaking'                : false, // true or false (if it is true, CSS 'page-break-before/after: always;' will work, partially)
+"page-breaking"                : false, // true or false (if it is true, CSS "page-break-before/after: always;" will work, partially)
 
 // -------------------------------------------------------------------------------------------------
 
-'google-analytics-tracking-id' : '', // Your own Google Analytics tracking id, as 'UA-********-*'
+"google-analytics-tracking-id" : "", // Your own Google Analytics tracking id, as "UA-********-*"
 
-'epub-additional-stylesheet'   : '', // path from spine-item or http:// URI or ''
-'epub-additional-script'       : '', // path from spine-item or http:// URI or ''
+"epub-additional-stylesheet"   : "", // path from spine-item or http:// URI or ""
+"epub-additional-script"       : "", // path from spine-item or http:// URI or ""
 
 // =================================================================================================
 
-'from-bibi' : '2Uw0' });
+"from-bibi" : "2Uw0" });
